2193bf88089574e02e90e22382b459222ec1832b
Szymon <szymonmaksymowicz2002@gmail.com>
2023-11-23 11:10:17 +0100

    First prototype of carousel working

--- lib/widgets/carouselElement.dart
+++ lib/widgets/carouselElement.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 
 class CarouselElement extends StatelessWidget {
-  const CarouselElement({Key? key}) : super(key: key);
+  final AssetImage image;
+  final String title;
+
+  const CarouselElement({Key? key, required this.image, required this.title})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
         width: 177,
         height: 276,
-        margin: EdgeInsets.symmetric(horizontal: 24),
+        margin: EdgeInsets.symmetric(horizontal: 21),
         padding: EdgeInsets.all(8),
         decoration: BoxDecoration(
-          color: Colors.red,
+          color: Theme.of(context).primaryColor,
           borderRadius: BorderRadius.circular(26),
         ),
-        child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(19),
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage(
-                        "assets/images/background_temp_image.png")))));
+        child: Column(
+          children: [
+            Container(
+                height: 211,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(19),
+                    image: DecorationImage(fit: BoxFit.cover, image: image))),
+            Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+            )
+          ],
+        ));
   }
 }
--- lib/widgets/homePageCarousel.dart
+++ lib/widgets/homePageCarousel.dart
@@ -2,20 +2,55 @@ import 'package:flutter/material.dart';
 
 import 'carouselElement.dart';
 
-class HomePageCarousel extends StatelessWidget {
+class HomePageCarousel extends StatefulWidget {
+
+
+  HomePageCarousel({Key? key})
+      : super(key: key);
+
+  @override
+  State<HomePageCarousel> createState() => _HomePageCarouselState();
+}
+
+
+
+class _HomePageCarouselState extends State<HomePageCarousel> {
+  List<AssetImage> images = [AssetImage("./assets/images/loading_dummy.jpg")];
+  List<String> titles = ["Loading"];
+
   @override
   Widget build(BuildContext context) {
+    genHomePageCarousel();
     return Container(
       height: 300, 
-      child: ListView(
+      child: ListView.builder(
+        itemCount: images.length,
         scrollDirection: Axis.horizontal,
-        children: <Widget>[
-          CarouselElement(),
-          CarouselElement(),
-          CarouselElement(),
-          CarouselElement(),
-        ],
+        prototypeItem: CarouselElement(image: images.first, title: titles.first),
+
+        itemBuilder: (context, index) {
+          return CarouselElement(image: images[index], title: titles[index]);
+        },
+
       ),
     );
   }
+
+  Future<void> genHomePageCarousel() async{
+    await Future.delayed(Duration(seconds: 3));
+    List<AssetImage> images = List.empty(growable: true);
+    List<String> titles = ["Aurora over Canada", "Galaxies", "Dust and the Western Veil Nebula", "PDS 70: Disk, Planets, and Moons"];
+    for(int i = 1; i < 5; i++){
+    images.add(AssetImage("./assets/images/pic$i.jpg"));
+  }
+  setState(() {
+    this.images = images;
+    this.titles = titles;
+  });
+  
 }
+}
+
+// import 'package:flutter/material.dart';
+// import 'package:space_view/widgets/homePageCarousel.dart';
+
