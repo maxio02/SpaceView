77d66ec7851ddff63d8f5d9c3f11a092aa9c9030
Valentina Bolbas <valentina.bolbas@gmail.com>
2023-12-12 23:06:02 +0100

    Corrected rendering of gallery full screen page and settings widget

--- lib/pages/fullscreenArticlePage.dart
+++ lib/pages/fullscreenArticlePage.dart
@@ -5,47 +5,71 @@ class FullscreenArticleScreen extends StatelessWidget {
   final String title;
   final String description;
 
-  const FullscreenArticleScreen(
-      {Key? key,
-      required this.image,
-      required this.title,
-      required this.description})
-      : super(key: key);
+  const FullscreenArticleScreen({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.description,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor
-        ),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
           Container(
-               width: screenSize.width,
-               height: screenSize.width,
-                decoration: BoxDecoration(
-                    image: DecorationImage(fit: BoxFit.cover, image: image))),
-            SizedBox(height: 10),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                textAlign: TextAlign.left,
+            width: screenSize.width,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              width: screenSize.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: screenSize.width,
+                    height: screenSize.width,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: image,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          description,
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.left,
+                        ),
+                        SizedBox(
+                            height: 20), // Add additional spacing as needed
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                description,
-                style: TextStyle(fontSize: 16),
-                textAlign: TextAlign.left,
-              )
-            ],)
-            )
-           
-              
-              ]),
+            ),
+          ),
+        ],
       ),
     );
   }
